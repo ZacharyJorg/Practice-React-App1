066b63a8a8842f2199830d0e03aa8b5706e55904
Zachary F Jorgensen <jorgensenzachary00@gmail.com>
2024-03-08 20:30:16 -0800

    accordian files created and added dummy data

---
+++ Practice-React1/src/components/accordian/data.ts
@@ -0,0 +1,33 @@
+interface Item {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const dummyItems: Item[] = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "What is the largest mammal?",
+    answer: "Blue whale",
+  },
+  {
+    id: 3,
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    answer: "Harper Lee",
+  },
+  {
+    id: 4,
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
+  },
+  {
+    id: 5,
+    question: "What year did the Titanic sink?",
+    answer: "1912",
+  },
+];
